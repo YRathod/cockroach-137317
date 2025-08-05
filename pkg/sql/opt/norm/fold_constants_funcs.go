8e62dd2b6d250aabf3fb932dce29629280472d4f
YRathod <YRathod@users.noreply.github.com>
2025-08-05 19:58:44 -0400

    Update fold_constants_funcs.go

--- pkg/sql/opt/norm/fold_constants_funcs.go
+++ pkg/sql/opt/norm/fold_constants_funcs.go
@@ -718,6 +718,8 @@ func (c *CustomFuncs) FoldAnyWithConst(cmp opt.Operator, left, right opt.ScalarE
 		elems = e.Elems
 	case *memo.ArrayExpr:
 		elems = e.Elems
+	case *memo.NullExpr:
+    return c.f.ConstructFalse(), true
 	default:
 		return nil, false
 	}
